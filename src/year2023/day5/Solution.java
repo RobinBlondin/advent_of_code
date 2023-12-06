abc8b69e216b39f9602859ffaf9644cb69331a9b
Robin Blondin <robin.blondin@protonmail.com>
2023-12-06 10:06:47 +0100

    Kept trying to finish part2, not yet finished

--- src/year2023/day5/Solution.java
+++ src/year2023/day5/Solution.java
@@ -16,12 +16,27 @@ public class Solution {
         converter = new ArrayList<>();
         newSeeds = new ArrayList<>();
         stage = Stages.SEEDS;
+
         readFile();
+        setNewSeeds();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-       solution.part1();
+        List<Long> list = new ArrayList<>();
+        long low = 2;
+        long high = 200000000;
+        long result = 0;
+        while(low < high) {
+            long mid = (low + high) / 2;
+            if (solution.part2(mid)) {
+                list.add(mid);
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println(Collections.min(list));
     }
 
     public void part1() {
@@ -34,7 +49,6 @@ public class Solution {
                 setStage(s);
                 continue;
             }
-
             long[] longs = Arrays.stream(s.split(" ")).mapToLong(Long::parseLong).toArray();
 
             for (int j = 0; j < seeds.length; j++) {
@@ -46,6 +60,48 @@ public class Solution {
         }
         long min = Arrays.stream(seeds).min().getAsLong();
         System.out.println(min);
+    }
+
+    public boolean part2(long input) {
+        boolean found = false;
+        boolean foundMatch = false;
+        /*outerLoop:
+        for (long m = 10000000; m < 20000000; m++) {*/
+            long number = input;
+            for (int i = converter.size() - 1; i >= 0; i--) {
+
+                if (converter.get(i).isEmpty()) {
+                    continue;
+                } else if (Character.isLetter(converter.get(i).charAt(0))) {
+                    setStage(converter.get(i));
+                    found = false;
+                    continue;
+                } else if (found) {
+                    continue;
+                }
+
+                long[] longs = Arrays.stream(converter.get(i).split(" ")).mapToLong(Long::parseLong).toArray();
+
+                if (number < longs[0] || number - longs[0] + longs[1] >= longs[1] + longs[2]) {
+                    continue;
+                }
+
+                number = number - longs[0] + longs[1];
+                found = true;
+
+                if (stage == Stages.FERTILIZER) {
+                    for (int j = 0; j < seeds.length - 1; j += 2) {
+                        if (number >= newSeeds.get(j) && number < newSeeds.get(j + 1)) {
+                            System.out.println("found match: " + input);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+       /*}
+        if (!foundMatch)
+            System.out.println("no match found: ");*/
     }
 
     public void readFile() {
@@ -66,10 +122,9 @@ public class Solution {
     }
 
     public void setNewSeeds() {
-        for (int i = 0; i < seeds.length; i+=2) {
-            for (long j = seeds[i]; j < seeds[i]+seeds[i+1]; j++) {
-                newSeeds.add(j);
-            }
+        for (int i = 0; i < seeds.length - 1; i+=2) {
+            newSeeds.add(seeds[i]);
+            newSeeds.add(seeds[i] + seeds[i+1] - 1);
         }
     }
 
